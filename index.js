5e5e74780a03f4cb49398db62e7a4a2ed70ddf2f
AFVA7 <andresf.valenciaa@uqvirtual.edu.co>
2023-12-10 09:36:53 -0500

    versiión inicial

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+const { dbConnection } = require('./db/config');
+
+
+//crear el server express
+const app = express();
+
+//base de datos
+dbConnection();
+
+// CORS
+app.use(cors());
+
+//directorio publico
+app.use(express.static('public'));
+
+//lectura y parseo del body
+app.use(express.json());
+
+//rutas
+app.use('/api/auth', require('./routes/auth'));
+//escuchar peticiones
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
+});
---
+++ mongo.md
@@ -0,0 +1,2 @@
+db user: mern_user
+db password: 1VuXgNzIjh1UDhNg
